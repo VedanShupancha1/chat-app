667b7bc98b31b2ecbd0a2195534b78767cea8ab0
VedanShupancha1 <90923210+VedanShupancha1@users.noreply.github.com>
2021-09-17 21:49:26 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+// Import modules
+const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
+// Create a server
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+// Serve static files (mainly for index.html)
+// index.html can now be accessed at http://localhost:PORT/
+app.use(express.static('public'));
+
+// Listen for new connections from different clients
+io.on('connection', socket => {
+    console.log('New user connected');
+
+    // Listen for 'message' event
+    socket.on('message', data => {
+        io.emit('message', data); // Emit the same event back to all connected clients
+    });
+});
+
+// Start the server
+const PORT = 3000;
+httpServer.listen(PORT, () => console.log(`Listening at port ${PORT}`));
